f1ed2c50803411a910496962edeedb03defad7e8
pliavi <pliavi.rpgm@gmail.com>
2020-08-10 10:56:57 -0300

    create RoundedButton component

---
+++ src/components/buttons/RoundedButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="RoundedButton">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.RoundedButton {
+  display: flex;
+  width: 42px;
+  height: 42px;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+  font-size: 2.5rem;
+  transition: all 200ms ease;
+  border-radius: 50%;
+
+  &:active {
+    background: var(--shadow-gray);
+  }
+}
+
+.-primary {
+  width: 58px;
+  height: 58px;
+  background-color: var(--background-primary);
+  color: var(--color-white);
+  box-shadow: 0 10px 20px var(--shadow-primary);
+
+  &:active {
+    background: var(--background-primary-light);
+  }
+}
+</style>

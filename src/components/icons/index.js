9e471176d46326e24f18e610a9e05731356e0a99
pliavi <pliavi.rpgm@gmail.com>
2020-08-10 10:56:36 -0300

    add icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,5 @@
+export { default as FeedIcon } from "./FeedIcon";
+export { default as SearchIcon } from "./SearchIcon";
+export { default as PlusIcon } from "./PlusIcon";
+export { default as ChatIcon } from "./ChatIcon";
+export { default as ProfileIcon } from "./ProfileIcon";

03c7ace9b93434066aa2a394fe688a21bbd8fcc5
pliavi <pliavi.rpgm@gmail.com>
2020-08-10 10:55:07 -0300

    add normalize and some variables

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import "normalize.css";
+import "./assets/variables.css";
 
 Vue.config.productionTip = false;
 

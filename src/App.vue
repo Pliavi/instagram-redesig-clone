ed0745e1eeeab5851f1a03ada6cbc99606ecff0c
pliavi <pliavi.rpgm@gmail.com>
2020-08-10 17:31:21 -0300

    remove app styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-view />
     <Navigation />
   </div>
 </template>
@@ -12,12 +13,3 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>

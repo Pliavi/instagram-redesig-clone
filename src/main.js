6f31a448fa74aa2eb7458ea5db2f3d6e26b04c61
pliavi <pliavi.rpgm@gmail.com>
2020-08-12 08:39:21 -0300

    install fontawesome and swiper

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from "vue";
+import "./plugins/fontawesome";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import "normalize.css";
 import "./assets/global.scss";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/swiper-bundle.css";
+
+Vue.use(VueAwesomeSwiper /* { default options with global component } */);
 
 Vue.config.productionTip = false;
-
 new Vue({
   router,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount("#app");

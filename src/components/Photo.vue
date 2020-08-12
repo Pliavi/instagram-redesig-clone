4ee78136e04cf6cc6840c1ac14852bf966767d20
pliavi <pliavi.rpgm@gmail.com>
2020-08-12 08:42:40 -0300

    improved blur effect in Photo component

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Photo">
-    <img class="Photo__image -blured" :src="src" />
-    <img class="Photo__image" :src="src" />
+    <img class="Photo__image -blured" :src="src" draggable="false" />
+    <img class="Photo__image" :src="src" draggable="false" />
   </div>
 </template>
 
@@ -14,23 +14,26 @@ export default {
 
 <style lang="scss" scoped>
 .Photo {
-  position: relative;  
+  position: relative;
+  width: 100%;
+  height: 100%;
+
   &__image {
     border-radius: var(--radius-md);
     width: 100%;
     height: 100%;
+    max-height: 90vh;
     object-fit: cover;
   }
 
   &__image.-blured {
     position: absolute;
     z-index: -1;
-    height: 50%;
     width: calc(100% - var(--padding-md) * 2);
     left: var(--padding-md);
-    bottom: var(--padding-md);
-    filter: blur(var(--padding-md));
-    opacity: .75;
+    bottom: 0;
+    filter: blur(20px) saturate(3);
+    opacity: 0.65;
   }
 }
 </style>

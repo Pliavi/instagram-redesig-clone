b379a436a9ba6f2fed961b99d4e6aae4186a939f
pliavi <pliavi.rpgm@gmail.com>
2020-08-12 08:40:06 -0300

    create HeaderBar component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
+    <HeaderBar />
     <router-view />
-    <Navigation />
+    <NavigationBar />
   </div>
 </template>
 
 <script>
-import Navigation from "./components/Navigation";
+import NavigationBar from "./components/NavigationBar";
+import HeaderBar from "./components/HeaderBar";
 
 export default {
-  components: { Navigation }
+  components: { HeaderBar, NavigationBar }
 };
 </script>
 
+<style lang="scss">
+#app {
+  margin-bottom: 72px; /* It's the navigation height */
+  margin-top: 52px; /* It's the header height */
+}
+</style>
---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="HeaderBar">
+    <img class="HeaderBar__logo" src="@/assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.HeaderBar {
+  position: fixed;
+  box-sizing: border-box;
+  width: 100%;
+  background: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #ddd;
+  box-shadow: 0 -1px 6px var(--shadow-gray);
+  padding: 0.75rem 0 0.5rem;
+  &__logo {
+    height: 32px;
+  }
+}
+</style>

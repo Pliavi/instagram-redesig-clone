644ee4654516218fdd3baece1bfe7e3bb33bca6c
pliavi <pliavi.rpgm@gmail.com>
2020-08-18 20:48:26 -0300

    make HeaderBar "templatable"

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="HeaderBar">
-    <img class="HeaderBar__logo" src="@/assets/logo.png" />
+    <div class="HeaderBar__left"><slot name="left"></slot></div>
+    <div class="HeaderBar__middle"><slot></slot></div>
+    <div class="HeaderBar__right"><slot name="right"></slot></div>
   </div>
 </template>
 
@@ -10,9 +12,11 @@ export default {};
 
 <style lang="scss" scoped>
 .HeaderBar {
+  display: flex;
   position: fixed;
   box-sizing: border-box;
   width: 100%;
+  height: 52px;
   background: white;
   z-index: 100;
   top: 0;
@@ -21,9 +25,35 @@ export default {};
   justify-content: center;
   border-bottom: 1px solid #ddd;
   box-shadow: 0 -1px 6px var(--shadow-gray);
-  padding: 0.75rem 0 0.5rem;
-  &__logo {
-    height: 32px;
+  padding: 0.75rem;
+
+  &__left,
+  &__right,
+  &__middle {
+    display: flex;
+    align-items: center;
+    justify-items: center;
+    &.-align-left{
+      justify-items: flex-start;
+    }
+    &.-align-right{
+      justify-items: flex-end;
+    }
+    &.-align-center{
+      justify-items: center;
+    }
+  }
+
+  &__left,
+  &__right {
+    flex: 0 1;
+  }
+  &__middle {
+    flex: 1;
+    margin: 0 var(--margin-md);
+  }
+  &__right {
+    text-align: right;
   }
 }
 </style>

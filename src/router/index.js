a078e31403ddc191810c5269237f85db06954493
pliavi <pliavi.rpgm@gmail.com>
2020-08-18 21:48:43 -0300

    remove About page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Notifications from "../views/Notifications.vue";
 
 Vue.use(VueRouter);
 
@@ -11,20 +12,17 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/notifications",
+    name: "Notifications",
+    component: Notifications
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
+
 
 export default router;

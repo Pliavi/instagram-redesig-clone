fc03bc26e9ff5721dcaf8a976c27675566f99219
pliavi <pliavi.rpgm@gmail.com>
2020-08-18 21:48:28 -0300

    change RoundedButton to 'component' component

--- src/components/buttons/RoundedButton.vue
+++ src/components/buttons/RoundedButton.vue
@@ -1,26 +1,48 @@
 <template>
-  <div class="RoundedButton">
+  <component
+    :is="tag"
+    v-bind="$attrs"
+    class="RoundedButton"
+    active-class="-active"
+    exact-active-class="-exact-active"
+  >
     <slot></slot>
-  </div>
+  </component>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: "button"
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .RoundedButton {
   display: flex;
+  box-sizing: border-box;
+  border: 0;
+  background: transparent;
   align-items: center;
   justify-content: center;
-  font-size: 2.5rem;
+  font-size: 100%;
   transition: all 200ms ease;
   border-radius: var(--radius-full);
   padding: var(--padding-md);
+  color: var(--text-light);
 
   &:active {
     background: var(--shadow-gray);
+    color: var(--text-color);
   }
+}
+
+.RoundedButton.-exact-active ::v-deep > * {
+  color: var(--text-color);
 }
 
 .-primary {

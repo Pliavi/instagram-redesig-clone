aa0a12eaa20de4a211e4443bd3ee719394563e27
pliavi <pliavi.rpgm@gmail.com>
2020-08-21 11:15:42 -0300

    improve truncatable text in PostCard

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -6,14 +6,18 @@
         <img class="UserPhoto__image" src="https://placekitten.com/64/64" />
       </div>
       <div class="ProfileLine__user-info">
-        <div class="ProfileLine__name">{{ metadata.user }}</div>
-        <div class="ProfileLine__location">{{ metadata.location }}</div>
+        <div class="ProfileLine__name">{{ postData.user }}</div>
+        <div class="ProfileLine__location">{{ postData.location }}</div>
       </div>
     </div>
 
-    <Photo v-if="photos.length === 1" :src="photos[0]" />
+    <Photo
+      v-if="postData.photos.length === 1"
+      :src="postData.photos[0]"
+      class="MySwiper"
+    />
     <swiper v-else class="MySwiper" ref="mySwiper" :options="swiperOptions">
-      <swiper-slide v-for="(photo, index) in photos" :key="index">
+      <swiper-slide v-for="(photo, index) in postData.photos" :key="index">
         <Photo :src="photo" />
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
@@ -23,14 +27,18 @@
     </swiper>
 
     <div class="ActionBox">
-      <div class="Action">
-        <font-awesome-icon class="Action__icon" :icon="['far', 'heart']" />
-        <span>{{ metadata.counters.likes }}</span>
+      <div class="Action" @click="liked = !liked">
+        <font-awesome-icon
+          class="Action__icon"
+          :class="{ '-liked': liked }"
+          :icon="[liked ? 'fas' : 'far', 'heart']"
+        />
+        <span>{{ postData.counters.likes }}</span>
       </div>
 
       <div class="Action">
         <font-awesome-icon class="Action__icon" :icon="['far', 'comment']" />
-        <span>{{ metadata.counters.comments }}</span>
+        <span>{{ postData.counters.comments }}</span>
       </div>
 
       <div class="Action">
@@ -38,7 +46,7 @@
           class="Action__icon"
           :icon="['far', 'share-square']"
         />
-        <span>{{ metadata.counters.shares }}</span>
+        <span>{{ postData.counters.shares }}</span>
       </div>
 
       <div class="Action">
@@ -48,19 +56,24 @@
 
     <div
       class="Subtitle"
-      :class="{ '-trucated': truncatedSubtitle }"
       @click="truncatedSubtitle = false"
+      ref="truncatableSubtitle"
     >
-      <a :href="metadata.user" class="Subtitle__user-nick">{{
-        metadata.user
-      }}</a>
-      &nbsp;
-      <span class="Subtitle__text">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quam
-        quia, soluta debitis magnam blanditiis odit voluptates non nobis rem
-        necessitatibus iure! Quam culpa sint temporibus natus consequuntur nisi
-        earum.
-      </span>
+      <div :class="{ '-trucated': truncatedSubtitle }">
+        <a :href="postData.user" class="Subtitle__user-nick">{{
+          postData.user
+        }}</a>
+        &nbsp;
+        <span class="Subtitle__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quam
+          quia, soluta debitis magnam blanditiis odit voluptates non nobis rem
+          necessitatibus iure! Quam culpa sint temporibus natus consequuntur
+          nisi earum.
+        </span>
+      </div>
+      <div class="Subtitle__see-more" v-if="truncatedSubtitle">
+        ver mais
+      </div>
     </div>
   </div>
 </template>
@@ -69,10 +82,12 @@
 import Photo from "@/components/Photo";
 
 export default {
-  props: ["metadata", "photos"],
+  props: ["postData"],
   data() {
     return {
-      truncatedSubtitle: true,
+      liked: false,
+      photosLoading: 0,
+      truncatedSubtitle: false,
       swiperOptions: {
         spaceBetween: 30,
         pagination: {
@@ -84,6 +99,15 @@ export default {
         }
       }
     };
+  },
+  mounted() {
+    const rem = parseInt(getComputedStyle(document.documentElement).fontSize);
+    const $sub = this.$refs.truncatableSubtitle;
+
+    // Two lines as in line-clamp in css (the .5 is for the possible margin)
+    if ($sub.offsetHeight > 2.5 * rem) {
+      this.truncatedSubtitle = true;
+    }
   },
   components: { Photo }
 };
@@ -131,6 +155,16 @@ export default {
   &__icon {
     width: 20px;
     height: 20px;
+
+    &.-liked {
+      transform: scale(1);
+      animation: bumpheart 200ms;
+      animation-iteration-count: 2;
+      animation-direction: alternate;
+      animation-timing-function: ease;
+      animation-fill-mode: both;
+      color: var(--color-primary);
+    }
   }
   &:last-child {
     margin-left: auto;
@@ -142,21 +176,26 @@ export default {
 }
 
 .Subtitle {
-  display: -webkit-box;
   box-sizing: border-box;
   text-align: left;
   padding: 0 var(--padding-md);
   border-radius: var(--radius-md);
-  &.-trucated {
+  .-trucated {
+    display: -webkit-box;
     overflow-y: hidden;
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
+    cursor: pointer;
   }
 
   &__user-nick {
     text-decoration: none;
     font-weight: bold;
     color: var(--link-color);
+  }
+
+  &__see-more {
+    color: var(--text-light);
   }
 
   &:active {
@@ -170,6 +209,15 @@ export default {
     position: relative;
     bottom: 0;
     z-index: 10;
+  }
+}
+
+@keyframes bumpheart {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.2);
   }
 }
 </style>

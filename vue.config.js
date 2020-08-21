53ecce8e60e1a80e1e21dd0d9bc950c6d08e326c
pliavi <pliavi.rpgm@gmail.com>
2020-08-21 11:12:10 -0300

    changing all icons and logo

--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,37 @@
 module.exports = {
   pwa: {
-    name: "Instagram Redesign",
+    name: "SomePhotos",
     themeColor: "#E91E63",
     msTileColor: "#E91E63",
     iconPaths: {
-      favicon32: "favicon32x32.png",
-      favicon16: "favicon64x64.png",
-      appleTouchIcon: "favicon152x152.png",
+      favicon32: "img/favicon32x32.png",
+      favicon16: "img/favicon64x64.png",
+      appleTouchIcon: "img/favicon152x152.png",
       maskIcon: "img/icons/safari-pinned-tab.svg",
-      msTileImage: "favicon144x144.png"
+      msTileImage: "img/favicon144x144.png"
     },
     manifestOptions: {
-      name: "Instagram Redesign",
-      short_name: "NewInsta",
+      name: "SomePhotos",
+      short_name: "SomePhotos",
       icons: [
         {
-          src: "favicon192x192.png",
+          src: "img/favicon192x192.png",
           sizes: "192x192",
           type: "image/png"
         },
         {
-          src: "favicon512x512.png",
+          src: "img/favicon512x512.png",
           sizes: "512x512",
           type: "image/png"
         },
         {
-          src: "favicon192x192.png",
+          src: "img/favicon192x192.png",
           sizes: "192x192",
           type: "image/png",
           purpose: "maskable"
         },
         {
-          src: "favicon512x512.png",
+          src: "img/favicon512x512.png",
           sizes: "512x512",
           type: "image/png",
           purpose: "maskable"

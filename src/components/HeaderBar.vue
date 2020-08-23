147ee260eae3c18d61b96a65d5ceadf7f9c26cef
pliavi <pliavi.rpgm@gmail.com>
2020-08-23 20:47:31 -0300

    rewiriting HeaderBar to tailwind

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,59 +1,19 @@
 <template>
-  <div class="HeaderBar">
-    <div class="HeaderBar__left"><slot name="left"></slot></div>
-    <div class="HeaderBar__middle"><slot></slot></div>
-    <div class="HeaderBar__right"><slot name="right"></slot></div>
+  <div
+    class="fixed top-0 z-30 flex items-center justify-center w-full h-12 py-3 bg-white shadow"
+  >
+    <div class="flex items-center justify-center w-12 h-12">
+      <slot name="left"></slot>
+    </div>
+    <div class="flex-grow">
+      <slot></slot>
+    </div>
+    <div class="flex items-center justify-center w-12 h-12">
+      <slot name="right"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {};
 </script>
-
-<style lang="scss" scoped>
-.HeaderBar {
-  display: flex;
-  position: fixed;
-  box-sizing: border-box;
-  width: 100%;
-  height: 52px;
-  background: white;
-  z-index: 100;
-  top: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-bottom: 1px solid #ddd;
-  box-shadow: 0 -1px 6px var(--shadow-gray);
-  padding: 0.75rem;
-
-  &__left,
-  &__right,
-  &__middle {
-    display: flex;
-    align-items: center;
-    justify-items: center;
-    &.-align-left{
-      justify-items: flex-start;
-    }
-    &.-align-right{
-      justify-items: flex-end;
-    }
-    &.-align-center{
-      justify-items: center;
-    }
-  }
-
-  &__left,
-  &__right {
-    flex: 0 1;
-  }
-  &__middle {
-    flex: 1;
-    margin: 0 var(--margin-md);
-  }
-  &__right {
-    text-align: right;
-  }
-}
-</style>
